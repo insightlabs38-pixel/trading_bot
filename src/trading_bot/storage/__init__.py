4a00073f1b0963a58b7f5c047a432171ac028715
insightlabs38-pixel <insightlabs38@gmail.com>
2026-08-18 03:39:49 -0400

    Phase 2: export artifact manifest utilities

--- src/trading_bot/storage/__init__.py
+++ src/trading_bot/storage/__init__.py
@@ -15,9 +15,23 @@ from trading_bot.storage.base import (
 )
 from trading_bot.storage.factory import create_storage_backend
 from trading_bot.storage.local import LocalStorageBackend
+from trading_bot.storage.manifests import (
+    ArtifactManifest,
+    ArtifactManifestError,
+    ArtifactVerificationError,
+    build_artifact_manifest,
+    load_artifact_manifest,
+    manifest_key_for,
+    publish_artifact_with_manifest,
+    verify_artifact_manifest,
+    write_artifact_manifest,
+)
 from trading_bot.storage.s3 import S3StorageBackend
 
 __all__ = [
+    "ArtifactManifest",
+    "ArtifactManifestError",
+    "ArtifactVerificationError",
     "ChecksumMismatchError",
     "LocalStorageBackend",
     "ObjectNotFoundError",
@@ -28,6 +42,12 @@ __all__ = [
     "StorageObjectMetadata",
     "TransferTimeoutPolicy",
     "UnsafeStorageKeyError",
+    "build_artifact_manifest",
+    "load_artifact_manifest",
+    "manifest_key_for",
+    "publish_artifact_with_manifest",
+    "verify_artifact_manifest",
+    "write_artifact_manifest",
     "create_storage_backend",
     "normalize_storage_key",
     "sha256_file",
