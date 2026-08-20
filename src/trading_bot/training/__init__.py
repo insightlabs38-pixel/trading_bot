ecc796e84c639bfe75d7cc00c0aab36384989559
insightlabs38-pixel <insightlabs38@gmail.com>
2026-08-20 16:08:33 -0400

    Export Phase 5 training framework

---
+++ src/trading_bot/training/__init__.py
@@ -0,0 +1,63 @@
+"""Common architecture-independent training framework."""
+
+from trading_bot.training.checkpoint import (
+    CheckpointCorruptionError,
+    CheckpointError,
+    CheckpointIdentity,
+    CheckpointManager,
+    CheckpointRestore,
+    CheckpointResumeError,
+)
+from trading_bot.training.contracts import (
+    InferenceBenchmark,
+    ModelOutput,
+    TradingModel,
+    TrainingBatch,
+    benchmark_inference,
+    count_parameters,
+)
+from trading_bot.training.predictions import (
+    PredictionArtifact,
+    PredictionArtifactError,
+    PredictionRecord,
+    PredictionWriteResult,
+    predict_records,
+    write_prediction_artifact,
+)
+from trading_bot.training.trainer import (
+    NonFiniteTrainingError,
+    Trainer,
+    TrainerError,
+    TrainerRuntimeOptions,
+    TrainingHeartbeat,
+    TrainingState,
+    UnsupportedPrecisionError,
+)
+
+__all__ = [
+    "CheckpointCorruptionError",
+    "CheckpointError",
+    "CheckpointIdentity",
+    "CheckpointManager",
+    "CheckpointRestore",
+    "CheckpointResumeError",
+    "InferenceBenchmark",
+    "ModelOutput",
+    "NonFiniteTrainingError",
+    "PredictionArtifact",
+    "PredictionArtifactError",
+    "PredictionRecord",
+    "PredictionWriteResult",
+    "TradingModel",
+    "Trainer",
+    "TrainerError",
+    "TrainerRuntimeOptions",
+    "TrainingBatch",
+    "TrainingHeartbeat",
+    "TrainingState",
+    "UnsupportedPrecisionError",
+    "benchmark_inference",
+    "count_parameters",
+    "predict_records",
+    "write_prediction_artifact",
+]
